1449afbcc6ca5f995dea9613be7772d82665d4b0
Not-aHacker <lilicia.nau@gmail.com>
2021-04-03 11:10:27 +0200

    init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import com.lilicia.muehle.Board;
+public class Main {
+    public static void main(String[] args) {
+        Board board = new Board();
+        board.print();
+    }
+}
---
+++ src/main/java/com/lilicia/muehle/Board.java
@@ -0,0 +1,21 @@
+package com.lilicia.muehle;
+
+public class Board {
+        public void print() {
+            System.out.println("*__________*__________*");
+            System.out.println("|          |          |");
+            System.out.println("|   *______*______*   |");
+            System.out.println("|   |      |      |   |");
+            System.out.println("|   |   *__*__*   |   |");
+            System.out.println("|   |   |  |  |   |   |");
+            System.out.println("*___*___*     *___*___*");
+            System.out.println("|   |   |  |  |   |   |");
+            System.out.println("|   |   *__*__*   |   |");
+            System.out.println("|   |      |      |   |");
+            System.out.println("|   *______*______*   |");
+            System.out.println("|          |          |");
+            System.out.println("*__________*__________*");
+
+
+        }
+}
