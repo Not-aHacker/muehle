f01014fc030fcea61ebb3d5cf8788e4d6a7242b0
Kohpai <whcwhc78@gmail.com>
2021-04-03 14:47:38 +0200

    :sparkles: prompt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 import com.lilicia.muehle.Board;
+import com.lilicia.muehle.Stone;
+
 public class Main {
+
     public static void main(String[] args) {
         Board board = new Board();
-        board.print();
+        Scanner scanner = new Scanner(System.in);
+        Stone turn;
+        int pos;
+        for (int i = 0; i < 18; i++) {
+            board.print();
+
+            turn = i % 2 == 0 ? Stone.WHITE : Stone.BLACK;
+            System.out.print("Enter your position (1-24 for " + turn.name() + "): ");
+            pos = scanner.nextInt();
+            // board.placeStone(turn, pos);
+        }
+
+        System.out.println("End");
     }
 }
