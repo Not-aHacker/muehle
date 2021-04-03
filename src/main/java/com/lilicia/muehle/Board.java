8acb381efa679cbb1e7c889bcdeab4078c8b6680
Not-aHacker <lilicia.nau@gmail.com>
2021-04-03 14:28:35 +0200

    :sparkles:

--- src/main/java/com/lilicia/muehle/Board.java
+++ src/main/java/com/lilicia/muehle/Board.java
@@ -28,7 +28,15 @@ public class Board {
 
             lines.add(new Line(pos));
         }
+        int[] arr = {0,9,21,3,10,18,6,11,15,1,4,7,16,19,22,8,12,17,5,13,20,2,14,23};
+        for (int i = 0; i < 24; i += 3) {
+            List<Position> pos = new ArrayList<>();
+            pos.add(this.positions.get(arr[i]));
+            pos.add(this.positions.get(arr[i + 1]));
+            pos.add(this.positions.get(arr[i + 2]));
 
+            lines.add(new Line(pos));
+        }
         // 2nd loop
         // (1,10,22), (4,11,19), (7,12,16),
         // (2,5,8), (17,20,23), (9,13,18),
@@ -36,19 +44,19 @@ public class Board {
     }
 
     public void print() {
-        System.out.println("*__________*__________*");
-        System.out.println("|          |          |");
-        System.out.println("|   *______*______*   |");
-        System.out.println("|   |      |      |   |");
-        System.out.println("|   |   *__*__*   |   |");
-        System.out.println("|   |   |  |  |   |   |");
-        System.out.println("*___*___*     *___*___*");
-        System.out.println("|   |   |  |  |   |   |");
-        System.out.println("|   |   *__*__*   |   |");
-        System.out.println("|   |      |      |   |");
-        System.out.println("|   *______*______*   |");
-        System.out.println("|          |          |");
-        System.out.println("*__________*__________*");
+        System.out.println( positions.get(0).getChar() + "___________" + positions.get(1).getChar() + "___________" + positions.get(2).getChar());
+        System.out.println("|           |           |");
+        System.out.println("|   " + positions.get(3).getChar() + "_______" +positions.get(4).getChar()+"_______"+positions.get(5).getChar()+"   |");
+        System.out.println("|   |       |       |   |");
+        System.out.println("|   |   "+positions.get(6).getChar()+"___"+positions.get(7).getChar()+"___"+positions.get(8).getChar()+"   |   |");
+        System.out.println("|   |   |       |   |   |");
+        System.out.println(positions.get(9).getChar() + "___"+positions.get(10).getChar()+"___"+positions.get(11).getChar()+"       "+positions.get(12).getChar()+"___"+positions.get(13).getChar()+"___"+positions.get(14).getChar());
+        System.out.println("|   |   |       |   |   |");
+        System.out.println("|   |   "+positions.get(15).getChar()+"___"+positions.get(16).getChar()+"___"+positions.get(17).getChar()+"   |   |");
+        System.out.println("|   |       |       |   |");
+        System.out.println("|   "+positions.get(18).getChar()+"_______"+positions.get(19).getChar()+"_______"+positions.get(20).getChar()+"   |");
+        System.out.println("|           |           |");
+        System.out.println(positions.get(21).getChar()+"___________"+positions.get(22).getChar()+"___________"+positions.get(23).getChar());
     }
 
 }
